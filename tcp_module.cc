f5fcd29d07f765f5f5ac20d139a7c713aafc3d18
Clark <clarkonicolas@gmail.com>
2015-10-26 06:56:31 -0400

    progress update
    kill me

--- tcp_module.cc
+++ tcp_module.cc
@@ -84,23 +84,113 @@ int main(int argc, char * argv[]) {
 	    
 	    if (event.handle == mux) {
 		/* Handle IP packet */
-			Packet p;
-			unsigned short len;
+
+	    	/* Packet class includes methods for extracting portions of the payload */
+			Packet p;	// Consists of a list of packet Headers, a Buffer that represents the payload of the packet, and a list of packet Trailers
+			unsigned short len;	// Packet length
 			bool checksumok;
+			TCPHeader tcp_header; // TCPHeader - wraps the raw data of a TCP header into a convenient abstraction
+			IPHeader ip_header;	// IPHeader - provides convenient access to the fields of an IPv4 header
+
+			// However, it is possible to extract raw data from the headers, payload, and trailers of a packet
 			MinetReceive(mux,p);
-			Buffer payload = p.GetPayload();
-			cerr << payload.GetSize();
-			cerr << "B IP packet\n";
+
+			// The size of a TCP Header is 20 bytes.. I think
+			p.ExtractHeaderFromPayload<TCPHeader>(20);
+
+			// virtual Header FindHeader(Headers::HeaderType ht) const;
+			tcp_header = p.FindHeader(Headers :: TCPHeader);
+			ip_header = p.FindHeader(Headers :: IPHeader);
+
+			// bool IsCorrectChecksum(const Packet &p) const;
+			checksumok = tcp_header.IsCorrectChecksum(p);
+
+			Connection conn;
+			// ConnectionList stores a list (queue) of ConnectionToStateMappings
+			ConnectionList<TCPState> :: iterator connections_iterator = clist.FindMatching(conn);
+			// ConnectionToStateMapping maps connection addresses to TCP connection state (TCPState)
+			ConnectionToStateMapping<TCPState> & connstate = (*connections_iterator);
+
+			// Grabs current TCP state
+			unsigned int current_tcp_state = (connstate).state.GetState();
+
+			switch(current_tcp_state) {
+				case LISTEN: {
+					break;
+				}
+
+				case SYN_RCVD: {
+					break;
+				}
+
+				case SYN_SENT: {
+					break;
+				}
+
+				case SYN_SENT1: {
+					break;
+				}
+
+				case ESTABLISHED: {
+					break;
+				}
+
+				case SEND_DATA: {
+					break;
+				}
+
+				case CLOSE_WAIT: {
+					break;
+				}
+
+				case FIN_WAIT1: {
+					break;
+				}
+
+				case CLOSING: {
+					break;
+				}
+
+				case LAST_ACK: {
+					break;
+				}
+
+				case FIN_WAIT2: {
+					break;
+				}
+
+				case TIME_WAIT: {
+					break;
+				}
+			}
+
 	    }
 
 	    if (event.handle == sock) {
 		/* Handle socket request or response */
-			SockRequestResponse req;
-			MinetReceive(sock, req);
-			switch (req.type) {
-				case CONNECT:
-				cerr << "CONNECT\n";
+			SockRequestResponse request;
+			SockRequestResponse response;
+
+			MinetReceive(sock, request);
+			switch (request.type) {
+				case CONNECT: {
+					/*
+					cerr << "CONNECT\n";
+				
+					Packet p;
+				
+					ConnectionToStateMapping<TCPState> mapping;
+
+					// TCPState: LISTEN, SYN_RCVD, SYN_SENT, SYN_SENT1, ESTABLISHED, SEND_DATA, CLOSE_WAIT, FIN_WAIT1, CLOSING, LAST_ACK, DIN_WAIT2, TIME_WAIT
+					// Note: TCP states represent the state AFTER the departure or arrival of the segment (RFC 793)
+					// TCPState(unsigned int initialSequenceNum, unsigned int state, unsigned int timertries)
+					TCPState next_tcp_state = TCPState(1, SYN_SENT, 3); // What to assign to timertries? Just put 3 because 3 way handshake l0l
+
+					Packet packet_to_send;
+					*/
 					break;
+				}
+
 				case ACCEPT:
 				cerr << "ACCEPT\n";
 					break;
